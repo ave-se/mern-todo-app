af392a556452bfd93c600807e13baf55db541020
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-05-24 12:04:11 +0200

    Self closing elements for progress bar, tickicon

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -2,7 +2,9 @@ const ListItem = ({ task }) => {
   return (
     <div className="list-item">
       <div className="info-container">
+        <TickIcon />
         <p className="task-title">{task.title}</p>
+        <ProgressBar />
       </div>
       <p>{task.title}</p>
     </div>
