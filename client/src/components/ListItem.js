9207fa8c2b33b26d6666a1ef3a4cf401ada419d6
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-05-24 12:01:56 +0200

    Made new divs

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,6 +1,9 @@
 const ListItem = ({ task }) => {
   return (
     <div className="list-item">
+      <div className="info-container">
+        <p className="task-title">{task.title}</p>
+      </div>
       <p>{task.title}</p>
     </div>
   );
