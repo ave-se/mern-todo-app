27d22ef916fe5a80e49d30191cc21cfeddb7cf64
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-05-24 13:31:11 +0200

    Updated modal with functions, form,class names

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,31 @@
 const Modal = () => {
-  return <div></div>;
+  const mode = "edit";
+  const handleChange = () => {
+    console.log("changing!");
+  };
+
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} you task</h3>
+          <button>X</button>
+        </div>
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder="Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+          />
+          <input />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
