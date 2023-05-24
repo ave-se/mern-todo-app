363386ff1581863101860f921b3b47d1ce30212e
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2023-05-24 11:33:25 +0200

    conected backend and frontend using async

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from "react/cjs/react.production.min";
 import ListHeader from "./components/ListHeader";
-
+import { useEffect } from "react";
 const App = () => {
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => getData, []);
+
   return (
     <div className="app">
       <ListHeader listName={"🏝 Holiday tick list"} />
